ef2aee7b79c17e1e637c6167338d08d4de6ee0b0
SpaceEEC <wermail2002-kap@yahoo.de>
2017-07-15 21:10:46 +0200

    Improve exporting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { PluginConstructor } from 'yamdbf';
 
+import { LeaguePluginOptions } from './types';
 import { LeaguePlugin } from './LeaguePlugin';
 
+const league: (token: string, options?: LeaguePluginOptions) => PluginConstructor = LeaguePlugin.build;
+export { league as League }
 export { LeaguePlugin };
 export { LeaguePluginOptions, Region } from './types';
-export default LeaguePlugin.build;
+export default league;
